bff05db5f2c50d2a464fa91e9e0bd60c37c991ec
Long Nguyen <nlhlong@protonmail.com>
2021-01-04 23:03:27 +0100

    minor changes

--- src/DecisionTree/playground.ts
+++ src/DecisionTree/playground.ts
@@ -15,12 +15,9 @@ import * as d3 from 'd3';
 import 'seedrandom';
 // FIXME: Should use the custom-ml-cart alias.
 import {
-//   DecisionTreeClassifier as DTClassifier,
-//   DecisionTreeRegression as DTRegressor
-// } from '../../node_modules/custom-ml-cart/src/index';
   DecisionTreeClassifier as DTClassifier,
   DecisionTreeRegression as DTRegressor
-} from '../../../decision-tree-cart/src/index';
+} from '../../node_modules/custom-ml-cart/src/index';
 import * as Utils from '../utils';
 import { HeatMap } from '../heatmap';
 import { Tree } from '../tree';
